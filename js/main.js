b5d9c16d8a2d1729630ab51df84b2f8425d0ae3a
anrbl <133613567+anrbl@users.noreply.github.com>
2023-05-15 16:08:44 +0900

    풀페이지
    풀페이지 연습작

---
+++ js/main.js
@@ -0,0 +1,17 @@
+$(function () {
+
+    //var --> const, let
+    const option = {
+        anchors: ['01', '02', '03'], /* 주소에 번호를 붙이다 */
+
+        onLeave: function (index, nextIndex, direction) {
+            console.log(index, nextIndex, direction)
+            $('h1 span').text(nextIndex); /* $('해당 위치')에.text를(이 안에 있는걸로)출력한다.*/
+            $('nav li').eq(nextIndex - 1).addClass('on').siblings().removeClass('on');
+            $('.section').eq(nextIndex - 1).addClass('on').siblings().removeClass('on');
+        },
+    }
+    $('.main_full').fullpage(option);
+    $('.section').eq(0).addClass('on');
+
+})
